5c4abb9efe4d6b14dd079ba0b3bf7babb91b0595
PolianaNeves <poliana.ambrosio.campos@gmail.com>
2022-02-01 20:08:32 -0300

    add styling to header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { Outlet } from "react-router";
 import NavBar from "./components/NavBar";
+import './App.css'
 
 function App() {
   return (
     <>
-      <NavBar />
+      <div className='header'>
+        <h1 className='app-title'>Dashboard - Universal Reviews Analysis</h1>
+        <NavBar/>
+      </div>
       <Outlet />
     </>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import './NavBar.css'
 
 export default function NavBar(props) {
   return (
     <>
-      <nav>
+      <nav className='navbar'>
         <Link to="/branch">Branch</Link>
         <Link to="/ratings">Ratings</Link>
         <Link to="/year">Year</Link>
