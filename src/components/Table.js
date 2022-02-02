9afcc257555077309d6488c3733d02f26f122ccd
PolianaNeves <poliana.ambrosio.campos@gmail.com>
2022-02-02 20:06:25 -0300

    add table styles

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,29 @@
+import "./Table.css";
+
 export default function Table(props) {
   return (
-    <table>
-      <thead>
-        <tr>
-          {props.headers &&
-            props.headers.map((header, index) => {
-              return <th key={index}>{header}</th>;
+    <div className='section-table'>
+      <table>
+        <thead>
+          <tr>
+            {props.headers &&
+              props.headers.map((header, index) => {
+                return <th key={index}>{header}</th>;
+              })}
+          </tr>
+        </thead>
+        <tbody>
+          {props.rows &&
+            props.rows.map((row, index) => {
+              return (
+                <tr key={index}>
+                  <td className='td-label'>{row.label}</td>
+                  <td>{row.value}</td>
+                </tr>
+              );
             })}
-        </tr>
-      </thead>
-      <tbody>
-        {props.rows &&
-          props.rows.map((row, index) => {
-            return (
-              <tr key={index}>
-                <td>{row.label}</td>
-                <td>{row.value}</td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 }
